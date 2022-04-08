742fad52f9561252f9201d060d7ed27e985826a4
unknown <gallardoirg@gmail.com>
2022-04-07 23:44:31 -0500

    Initial Commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,75 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 6531605,
+    "boundingBox": {
+        "lx": 969299.3348083496,
+        "ly": 11995775.821411133,
+        "lz": 13.830399017333982,
+        "ux": 969896.7269897461,
+        "uy": 11996373.21359253,
+        "uz": 611.2225804138184
+    },
+    "tightBoundingBox": {
+        "lx": 969299.3348,
+        "ly": 11995775.8214,
+        "lz": 13.830399999999998,
+        "ux": 969896.727,
+        "uy": 11996331.148,
+        "uz": 73.2917
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "gps-time",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 5.173568248748779,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
